fd896c089c0e00b6d0762bfba3472509df368ed1
Oswaldo <U22209742@utp.edu.pe>
2025-04-11 16:14:21 -0500

    cambio de estilos para compenente relacionando a inputGroup

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,38 @@
 import './assets/main.css'
 import './assets/main.css'
-import 'primeicons/primeicons.css';
+import 'primeicons/primeicons.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 //importamos prime vue
-import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/aura';
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 import App from './App.vue'
 import router from './router'
 
-import { definePreset } from '@primeuix/themes';
-
+import { definePreset } from '@primeuix/themes'
 
 const app = createApp(App)
 
 const MyPreset = definePreset(Aura, {
   semantic: {
-      primary: {
+    primary: {
+      50: '{blue.50}',
+      100: '{blue.100}',
+      200: '{blue.200}',
+      300: '{blue.300}',
+      400: '{blue.400}',
+      500: '{blue.500}',
+      600: '{blue.600}',
+      700: '{blue.700}',
+      800: '{blue.800}',
+      900: '{blue.900}',
+      950: '{blue.950}',
+    },
+    colorScheme: {
+      light: {
+        surface: {
+          0: '#ffffff',
           50: '{blue.50}',
           100: '{blue.100}',
           200: '{blue.200}',
@@ -28,54 +43,47 @@ const MyPreset = definePreset(Aura, {
           700: '{blue.700}',
           800: '{blue.800}',
           900: '{blue.900}',
-          950: '{blue.950}'
-      },
-      colorScheme:{
-        light:{
-          surface:{
-            0: '#ffffff',
-            50: '{blue.50}',
-            100: '{blue.100}',
-            200: '{blue.200}',
-            300: '{blue.300}',
-            400: '{blue.400}',
-            500: '{blue.500}',
-            600: '{blue.600}',
-            700: '{blue.700}',
-            800: '{blue.800}',
-            900: '{blue.900}',
-            950: '{blue.950}'
-          }
+          950: '{blue.950}',
         },
-        dark:{
-          surface:{
-            0: '#ffffff',
-            50: '{slate.50}',
-            100: '{slate.100}',
-            200: '{slate.200}',
-            300: '{slate.300}',
-            400: '{slate.400}',
-            500: '{slate.500}',
-            600: '{slate.600}',
-            700: '{slate.700}',
-            800: '{slate.800}',
-            900: '{slate.900}',
-            950: '{slate.950}'
-          }
-        }
-      }
-  }
-});
+        formField: {
+          borderColor: '{neutral-400}',
+          placeholderColor:'{neutral-500}'
+        },
+      },
+      dark: {
+        surface: {
+          0: '#ffffff',
+          50: '{slate.50}',
+          100: '{slate.100}',
+          200: '{slate.200}',
+          300: '{slate.300}',
+          400: '{slate.400}',
+          500: '{slate.500}',
+          600: '{slate.600}',
+          700: '{slate.700}',
+          800: '{slate.800}',
+          900: '{slate.900}',
+          950: '{slate.950}',
+        },
+      },
+    },
+  },
+})
 
 app.use(createPinia())
 app.use(PrimeVue, {
-    theme: {
-      preset: MyPreset,
-      options:{
-        darkModeSelector:'.dark'
-            }
-    }
-});
+  theme: {
+    preset: MyPreset,
+    options: {
+      darkModeSelector: '.dark',
+      prefix: 'p',
+      cssLayer: {
+        name: 'primevue',
+        order: 'theme, base, primevue',
+      },
+    },
+  },
+})
 
 app.use(router)
 
--- src/views/IndexPage.vue
+++ src/views/IndexPage.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import  InputGroup  from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon'
+import InputText from 'primevue/inputtext'
+import ButtonTheme from '@/components/ButtonTheme.vue';
 </script>
 <template>
 <div>
+<ButtonTheme/>
 <h2>Bienvenido</h2>
+<InputGroup>
+    <InputGroupAddon class="text-neutral-400" >
+        <i class="pi pi-user"></i>
+    </InputGroupAddon>
+    <InputText  placeholder="Username" />
+</InputGroup>
 </div>
 </template>
