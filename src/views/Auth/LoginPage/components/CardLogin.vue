5209ebb34ae9984cab27eb7f43d85b87c27b428a
Oswaldo <U22209742@utp.edu.pe>
2025-04-17 19:49:23 -0500

    Envio de evento de cardLogin

--- src/views/Auth/LoginPage/components/CardLogin.vue
+++ src/views/Auth/LoginPage/components/CardLogin.vue
@@ -7,43 +7,65 @@ import Password from 'primevue/password'
 import Image from 'primevue/image'
 import LogoRose from '@/assets/images/logos/logo-rose.png'
 import Button from 'primevue/button'
+import { defineEmits, reactive } from 'vue'
+//emitter
+const emit = defineEmits(['emit-login'])
+
+//function that emit login action
+const doLogin = () => {
+  emit('emit-login',formLoginData)
+  console.log('Emitiendo',formLoginData)
+}
+
+const formLoginData = reactive({ email: '', password: '' })
 </script>
 
 <template>
-  <Card class="h-auto w-xl flex flex-col items-center justify-center text-neutral-950 dark:text-surface-0 dark:bg-surface-800">
+  <form @submit.prevent="doLogin()">
+  <Card
+    class="h-auto w-xl flex flex-col items-center justify-center text-neutral-950 dark:text-surface-0 dark:bg-surface-800"
+  >
     <!-- header for image -->
-    <template #header>     <Image :src="LogoRose" alt="Logo" width="220" /></template>
+    <template #header> <Image :src="LogoRose" alt="Logo" width="220" /></template>
 
     <template #title>
-      <h3 class="h3 text-center" >Iniciar Sesion</h3>
+      <h3 class="h3 text-center">Iniciar Sesion</h3>
     </template>
     <!-- for inputs -->
+
     <template #content>
       <!-- for inputs internal -->
-       <div class="flex flex-col gap-4">
-      <!-- for email -->
-       <p>Email</p>
-      <InputGroup class="h-auto w-md">
-        <InputGroupAddon class="text-neutral-400" >
-          <i class="pi pi-user"></i>
-        </InputGroupAddon>
-        <InputText type="email" placeholder="Email" />
-      </InputGroup>
-      <!-- for password -->
-      <p class="textBase">Contraseña</p>
-      <InputGroup >
-        <InputGroupAddon class="text-neutral-400">
-          <i class="pi pi-lock"></i>
-        </InputGroupAddon>
-        <Password toggleMask :feedback="false" placeholder="Contraseña" />
-      </InputGroup>
-       </div>
-
+      <div class="flex flex-col gap-4">
+        <!-- for email -->
+        <p>Email</p>
+        <InputGroup class="h-auto w-md">
+          <InputGroupAddon class="text-neutral-400">
+            <i class="pi pi-user"></i>
+          </InputGroupAddon>
+          <InputText v-model="formLoginData.email" type="email" placeholder="Email" />
+        </InputGroup>
+        <!-- for password -->
+        <p class="textBase">Contraseña</p>
+        <InputGroup>
+          <InputGroupAddon class="text-neutral-400">
+            <i class="pi pi-lock"></i>
+          </InputGroupAddon>
+          <Password v-model="formLoginData.password" toggleMask :feedback="false" placeholder="Contraseña" />
+        </InputGroup>
+      </div>
     </template>
     <!-- for bottons -->
     <template #footer>
-      <div></div>
-      <Button class="w-md mt-4" label="Ingresar" severity="success" icon="pi pi-sign-in" iconPos="right" />
+      <Button
+      type="submit"
+        class="w-md mt-4"
+        label="Ingresar"
+        severity="success"
+        icon="pi pi-sign-in"
+        iconPos="right"
+      />
     </template>
+
   </Card>
+</form>
 </template>
