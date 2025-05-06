684c4c55751e013aeb251bb3883364957d3adb57
Oswaldo <U22209742@utp.edu.pe>
2025-05-05 20:00:08 -0500

    fix:Arreglo en la validacion de dni y cmvp en schema-search-employee

--- src/validation-schemas-forms/schema-search-employee.ts
+++ src/validation-schemas-forms/schema-search-employee.ts
@@ -1,8 +1,8 @@
 import * as yup from 'yup';
 
 export const schema = yup.object({
-  dni: yup.string().matches(/^\d{1,8}$/, 'DNI debe tener entre 1 y 8 dígitos numéricos'),
-  cmvp:yup.string().matches(/^\d{1,8}$/, 'CMVP debe tener entre 1 y 8 dígitos numéricos'),
+  dni: yup.string().matches(/^\d{0,8}$/, 'DNI debe tener hasta 8 dígitos numéricos'),
+  cmvp:yup.string().matches(/^\d{0,8}$/, 'CMVP debe tener hasta 8 dígitos numéricos'),
   names:yup.string().max(50,'Nombres debe tener menos de 50 dígitos'),
   lastnames:yup.string().max(50,'Nombres debe tener menos de 50 dígitos'),
   rol:yup.number()
