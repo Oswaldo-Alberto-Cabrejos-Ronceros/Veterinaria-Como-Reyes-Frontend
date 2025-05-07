2e6efcdbfa161111fd4f5333ca4e1c41e65b61bf
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-05-07 14:54:50 -0500

    feat:Creacion de Interface Service, además de datos de prueba

---
+++ src/models/Service.ts
@@ -0,0 +1,12 @@
+export interface Service{
+    id:number,
+    name:string,
+    description:string,
+    price:number,
+    duration:string //format: HH/mm
+    dirImage:string,
+    specie:string,
+    specieId:number,
+    category:string,
+    categoryId:number
+}
