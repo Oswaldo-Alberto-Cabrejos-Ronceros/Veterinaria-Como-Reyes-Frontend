c45a8d3a2a6e23c0ccb7381c6ba1e7b05009b28c
Oswaldo <U22209742@utp.edu.pe>
2025-05-12 16:48:23 -0500

    feat:Creacion de ModulePermissionCard

---
+++ src/views/Employee/RolePermissionManagementPage/components/ModulePermissionCard.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Card from 'primevue/card'
+
+</script>
+
+<template>
+
+  <Card class="text-neutral-950 dark:text-surface-0 dark:bg-surface-800 px-4 py-4">
+
+  </Card>
+</template>
