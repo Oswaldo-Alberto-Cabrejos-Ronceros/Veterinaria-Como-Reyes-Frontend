9c61691133291f222a580de60f36404c3aca20b9
Oswaldo <U22209742@utp.edu.pe>
2025-05-24 15:41:31 -0500

    feat:Creacion de composable para manejar funciones asincronas que devuelve loading y error

---
+++ src/composables/useAsyncHandler.ts
@@ -0,0 +1,25 @@
+import { reactive } from 'vue'
+
+type LoadingState = Record<string, boolean>
+type ErrorState = Record<string, unknown>
+
+export function useAsyncHandler() {
+  // for loading state
+  const loading = reactive<LoadingState>({})
+  //for error state
+  const error = reactive<ErrorState>({})
+  //higher orden fuction for run use case
+  async function runUseCase<T>(name: string, fn: () => Promise<T>) {
+    loading[name] = true
+    error[name] = null
+    try {
+      const result = await fn()
+      return result
+    } catch (e) {
+      error[name] = e
+    } finally {
+      loading[name] = false
+    }
+  }
+  return { loading, error, runUseCase }
+}
