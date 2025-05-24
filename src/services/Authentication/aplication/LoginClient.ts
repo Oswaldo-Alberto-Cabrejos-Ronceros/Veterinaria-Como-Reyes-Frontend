d9e9f5b4dc094788fdb1477d4c2f2760138fdc3b
Oswaldo <U22209742@utp.edu.pe>
2025-05-24 14:49:02 -0500

    fix:Eliminacion de llamado de store de autenticacion en los casos de uso de Authentication
    Debido a principios de clean Arquitecture

--- src/services/Authentication/aplication/LoginClient.ts
+++ src/services/Authentication/aplication/LoginClient.ts
@@ -1,14 +1,9 @@
 import type { AuthenticationService } from '../domain/services/AuthenticationService'
-import { useAuthenticationStore } from '@/stores/authenticationStore'
 
 export class LoginClient {
   constructor(private authenticationService: AuthenticationService) {}
   async execute(email: string, password: string) {
     const user = await this.authenticationService.loginClient(email, password)
-
-    //store
-    const authenticationStore = useAuthenticationStore()
-    authenticationStore.setUser(user)
     return user
   }
 }
--- src/services/Authentication/aplication/ResgisterUserClient.ts
+++ src/services/Authentication/aplication/ResgisterUserClient.ts
@@ -1,14 +1,11 @@
 import type { UserClientRequest } from '../domain/models/User'
 import type { AuthenticationService } from '../domain/services/AuthenticationService'
-import { useAuthenticationStore } from '@/stores/authenticationStore'
+
 export class RegisterUserClient {
   constructor(private authenticationService: AuthenticationService) {}
 
   async execute(userClient: UserClientRequest) {
     const userAuth = await this.authenticationService.register(userClient)
-
-    const authenticationStore = useAuthenticationStore()
-    authenticationStore.setUser(userAuth)
 
     return userAuth
   }
