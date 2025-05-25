d25c246d33222cb9e3dfeb908da2ed3e2ce6d198
Oswaldo <U22209742@utp.edu.pe>
2025-05-25 01:52:27 -0500

    feat:Creacion de la plantilla de EmployeeServiceImpl

---
+++ src/services/Employee/infrastructure/EmployeeServiceImpl.ts
@@ -0,0 +1,44 @@
+import type { Employee } from '@/models/Employee'
+import type { EmployeeService } from '../domain/services/EmployeeService'
+
+export class EmployeeServiceImpl implements EmployeeService {
+  getAllEmployees(): Promise<Employee[]> {
+    throw new Error('Method not implemented.')
+  }
+  getEmployeeById(employeeId: number): Promise<Employee> {
+    console.log(employeeId)
+    throw new Error('Method not implemented.')
+  }
+  createEmployee(employee: Employee): Promise<Employee> {
+    console.log(employee)
+    throw new Error('Method not implemented.')
+  }
+  updateEmployee(employee: Employee): Promise<Employee> {
+    console.log(employee)
+    throw new Error('Method not implemented.')
+  }
+  blockEmployee(employeeId: number): Promise<void> {
+    console.log(employeeId)
+    throw new Error('Method not implemented.')
+  }
+  restoreEmployee(employeeId: number): Promise<void> {
+    console.log(employeeId)
+    throw new Error('Method not implemented.')
+  }
+  searchEmployees(
+    dni?: string,
+    name?: string,
+    lastName?: string,
+    status?: boolean,
+    headquarterId?: number,
+    page?: number,
+    size?: number,
+  ): Promise<Employee[]> {
+    console.log(dni, name, lastName, status, headquarterId, page, size)
+    throw new Error('Method not implemented.')
+  }
+  getEmployeeMyInfo(employeeId: number): Promise<Employee> {
+    console.log(employeeId)
+    throw new Error('Method not implemented.')
+  }
+}
