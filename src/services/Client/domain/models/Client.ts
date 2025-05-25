98c337a574e36c36dab4579c09e3b30370c835cc
Oswaldo <U22209742@utp.edu.pe>
2025-05-25 17:25:43 -0500

    feat:Creacion del modelo Client en el modulo Cliente

---
+++ src/services/Client/domain/models/Client.ts
@@ -0,0 +1,20 @@
+export interface Client {
+  clientId: number
+  dni: string
+  name: string
+  lastName: string
+  address: string
+  phone: string
+  birthDate: string
+  dirImage: string
+  headquarter: {
+    headquarterId: number
+    name: string
+  }
+  user?: {
+    userId?: number
+    email: string
+    password?: string
+  }
+  blockNote?: string
+}
