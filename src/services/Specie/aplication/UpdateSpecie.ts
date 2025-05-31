8398c973b6146461ed41465ca1f30d306bb8bf47
Oswaldo <U22209742@utp.edu.pe>
2025-05-31 12:35:54 -0500

    feat:Creacion de container para species, ademas de arreglo en el caso de uso UpdateSpecie

--- src/services/Specie/aplication/UpdateSpecie.ts
+++ src/services/Specie/aplication/UpdateSpecie.ts
@@ -1,10 +1,10 @@
 import type { SpecieService } from '../domain/services/SpecieService'
 import type { Specie } from '../domain/models/Specie'
 
-export class GetAllSpecies {
+export class UpdateSpecie {
   constructor(private readonly service: SpecieService) {}
 
-  async execute(): Promise<Specie[]> {
-    return await this.service.getAllSpecies()
+  async execute(specieId: number, specie: Specie): Promise<Specie> {
+    return await this.service.updateSpecie(specieId, specie)
   }
 }
