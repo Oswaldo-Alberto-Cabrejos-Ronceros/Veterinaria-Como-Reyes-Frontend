b6c02997a8ceaefdc9fc46d0b11178408cbe379a
Oswaldo <U22209742@utp.edu.pe>
2025-05-31 18:27:29 -0500

    feat:Creacion de los metodos create y update en el compose Breed

--- src/services/Breed/aplication/CreateBreed.ts
+++ src/services/Breed/aplication/CreateBreed.ts
@@ -1,10 +1,10 @@
 import type { BreedService } from '../domain/services/BreedService'
-import type { Breed } from '../domain/models/Breed'
+import type { Breed, BreedRequest } from '../domain/models/Breed'
 
 export class CreateBreed {
   constructor(private readonly service: BreedService) {}
 
-  async execute(breed: Breed): Promise<Breed> {
-    return await this.service.createBreed(breed)
+  async execute(breedRequest: BreedRequest): Promise<Breed> {
+    return await this.service.createBreed(breedRequest)
   }
 }
