f5235b45d91db80aca5869573eb47a6350a96eb3
Oswaldo <U22209742@utp.edu.pe>
2025-05-31 14:43:04 -0500

    feat:Creacion de metodo para adaptar desde schemaAddEdit hacia SpecieRequest

--- src/adapters/SpecieAdapter.ts
+++ src/adapters/SpecieAdapter.ts
@@ -1,5 +1,6 @@
-import type { Specie } from '@/services/Specie/domain/models/Specie'
+import type { Specie, SpecieRequest } from '@/services/Specie/domain/models/Specie'
 import type { Specie as SpecieView } from '@/models/Specie'
+import type { FormValues as SpecieAddEditSchema } from '@/validation-schemas-forms/schema-add-edit-specie'
 
 export class SpecieAdapter {
   static toSpecieView(specie: Specie): SpecieView {
@@ -7,6 +8,13 @@ export class SpecieAdapter {
       id: specie.specieId,
       name: specie.name,
       imagePath: specie.imagePath,
+    }
+  }
+  //para adaptar de schema a SpecieRequest
+  static fromSchemaAddEditToSpecieRequest(schemaAddEdit: SpecieAddEditSchema): SpecieRequest {
+    return {
+      name: schemaAddEdit.name,
+      imagePath: schemaAddEdit.imagePath,
     }
   }
 }
