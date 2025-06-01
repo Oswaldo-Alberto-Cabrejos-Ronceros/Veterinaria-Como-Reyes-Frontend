9f9d8c4c1eee0b849059b117268d2409802cb7c8
Oswaldo <U22209742@utp.edu.pe>
2025-06-01 12:38:52 -0500

    fix:Arreglo en titulo del campo celular en AddClientCard

--- src/views/Employee/ClientManagementPage/components/AddClientCard.vue
+++ src/views/Employee/ClientManagementPage/components/AddClientCard.vue
@@ -59,7 +59,7 @@ const textFields: { title: string; key: keyof typeof fieldMap; icon: string }[] 
     icon: 'pi-user',
   },
   {
-    title: 'Phone',
+    title: 'Celular',
     key: 'phone',
     icon: 'pi-mobile',
   },
