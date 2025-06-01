edfc90f7bf51959beaba33b8709c62ca5eba8277
Oswaldo <U22209742@utp.edu.pe>
2025-06-01 15:12:09 -0500

    feat:Creacion de container para role

--- src/services/Role/aplication/GetAllRoles.ts
+++ src/services/Role/aplication/GetAllRoles.ts
@@ -1,7 +1,7 @@
 import type { Role } from "../domain/models/Role";
 import type { RoleService } from "../domain/services/RoleService";
 
-export class getAllRoles{
+export class GetAllRoles{
     constructor(private readonly roleService:RoleService){}
 
     async execute():Promise<Role[]>{
