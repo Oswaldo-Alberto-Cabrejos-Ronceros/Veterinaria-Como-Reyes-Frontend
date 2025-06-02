5355f6aa3cd30b55dd781d25495e4e95c1c99212
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-06-02 17:10:01 -0500

    feat:Creacion de metodos create y update en el compose Pet

--- src/composables/usePet.ts
+++ src/composables/usePet.ts
@@ -3,14 +3,24 @@ import { useAsyncHandler } from './useAsyncHandler'
 import { AnimalAdapter } from '@/adapters/AnimalAdapter'
 import type { Animal } from '@/services/Animal/domain/models/Animal'
 import type { Pet as PetView } from '@/models/Pet'
+import type { FormValues as PetAddEditSchema } from '@/validation-schemas-forms/schema-add-edit-pet'
 
 export function usePet() {
   const { loading, error, runUseCase } = useAsyncHandler()
 
   //create
 
-  const deletePet = async (animalId: number): Promise<void> => {
-    await runUseCase('deletePet', () => animalUsesCases.deleteAnimal.execute(animalId))
+  const createPet = async(petAddEditSchema:PetAddEditSchema):Promise<PetView>=>{
+    const animalRequest = AnimalAdapter.fromSchemaAddEditToPetRequest(petAddEditSchema);
+    const animal:Animal = await runUseCase('createPet',()=>
+    animalUsesCases.createAnimal.execute(animalRequest)
+    )
+    return AnimalAdapter.toPetView(animal)
+  }
+
+
+  const deletePet = async (petId: number): Promise<void> => {
+    await runUseCase('deletePet', () => animalUsesCases.deleteAnimal.execute(petId))
   }
 
   const getAllPets = async (): Promise<PetView[]> => {
@@ -28,21 +38,33 @@ export function usePet() {
     return animals.map((animal) => AnimalAdapter.toPetView(animal))
   }
 
-  const getPetById = async (animalId: number): Promise<PetView> => {
+  const getPetById = async (petId: number): Promise<PetView> => {
     const animal: Animal = await runUseCase('getPetById', () =>
-      animalUsesCases.getAnimalById.execute(animalId),
+      animalUsesCases.getAnimalById.execute(petId),
     )
     return AnimalAdapter.toPetView(animal)
   }
 
+
   //update
+
+    const updatePet = async(petId:number,petAddEditSchema:PetAddEditSchema):Promise<PetView>=>{
+    const animalRequest = AnimalAdapter.fromSchemaAddEditToPetRequest(petAddEditSchema);
+    const animal:Animal = await runUseCase('updatePet',()=>
+    animalUsesCases.updateAnimal.execute(petId,animalRequest)
+    )
+    return AnimalAdapter.toPetView(animal)
+  }
+
 
   return {
     loading,
     error,
+    createPet,
     deletePet,
     getAllPets,
     getPetByClientId,
     getPetById,
+    updatePet
   }
 }
