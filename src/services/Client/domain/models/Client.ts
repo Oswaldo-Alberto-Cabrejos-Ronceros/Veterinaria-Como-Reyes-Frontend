89dfaafe7e932bd0edf532ef02a2b8001c9163d6
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-06-03 12:12:04 -0500

    feat:Creacion de modeloMyInfoCliente en el servicio Cliente

--- src/services/Client/domain/models/Client.ts
+++ src/services/Client/domain/models/Client.ts
@@ -35,3 +35,21 @@ export interface ClientRequest {
     password: string
   }
 }
+
+
+export interface MyInfoClient{
+  clientId:number,
+  user:{
+        id:number
+    email:string
+  }
+  dni:string,
+  names:string,
+  LastNames:string,
+  phone:string,
+  address:string,
+  headquarter:{
+    id:number,
+    name:string
+  }
+}
