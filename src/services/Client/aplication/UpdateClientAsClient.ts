ceaabf417d4fcf9bad5b2118fbb41bcfcac90c9e
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-06-03 12:25:10 -0500

    fix:Cambios en el caso de uso UpdateClientAsCliente, para que reciba un ClientUpdateAsClient

--- src/services/Client/aplication/UpdateClientAsClient.ts
+++ src/services/Client/aplication/UpdateClientAsClient.ts
@@ -1,9 +1,9 @@
-import type { Client } from '../domain/models/Client'
+import type { Client, ClientUpdateAsClient } from '../domain/models/Client'
 import type { ClientService } from '../domain/services/ClientService'
 
 export class UpdateClientAsClient {
   constructor(private readonly clientService: ClientService) {}
-  async execute(clientId: number, client: Client): Promise<string> {
-    return await this.clientService.updateClientAsClient(clientId, client)
+  async execute(clientId: number, clientUpdateAsClient: ClientUpdateAsClient): Promise<string> {
+    return await this.clientService.updateClientAsClient(clientId, clientUpdateAsClient)
   }
 }
