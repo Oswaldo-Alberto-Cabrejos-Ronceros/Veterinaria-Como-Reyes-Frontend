0205a4c5caa9db56839e480b4aa12bc909d134be
Oswaldo <U22209742@utp.edu.pe>
2025-06-05 23:35:01 -0500

    feat:Creacion de MyPetsPage Cliente

---
+++ src/views/Client/MyPetsPage/MyPetsPage.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="layout-principal-flex">
+<Card class="card-principal-color-neutral">
+        <template #title>
+        <h3 class="h3">Bienvenido</h3>
+      </template>
+         <template #content>
+          <div class="flex flex-col gap-6">
+            
+          </div>
+         </template>
+</Card>
+  </div>
+</template>
+
+
+
