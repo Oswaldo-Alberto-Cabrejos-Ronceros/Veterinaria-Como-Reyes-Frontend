44163fa1be390b57996f694e3d9c6059c6176bc0
Oswaldo <U22209742@utp.edu.pe>
2025-06-13 16:00:19 -0500

    feat:Creacion de CardServiceSecondary e integracion en CardScheduleAppointmentPrimary
    Ademas integracion de Panel en la seccion horario en CardScheduleAppointment

--- src/components/CardScheduleAppointmentPrimary.vue
+++ src/components/CardScheduleAppointmentPrimary.vue
@@ -6,15 +6,39 @@ import Step from 'primevue/step'
 import StepPanels from 'primevue/steppanels'
 import StepPanel from 'primevue/steppanel'
 import Button from 'primevue/button'
+import CardPetPrimary from './CardPetPrimary.vue'
+import CardServiceSecondary from './CardServiceSecondary.vue'
+import Panel from 'primevue/panel'
+
+defineProps<{
+  pets: {
+    petId: number
+    petImageUrl: string
+    petName: string
+    petSpecie: string
+    petBreed: string
+    petGender: string
+    birthdate: string
+  }[]
+  services?: {
+    serviceId: number
+    serviceName: string
+    serviceImageUrl: string
+    specieName: string
+    categoryName: string
+    duration: string
+    price: number
+  }[]
+}>()
 </script>
 
 <template>
   <Card class="card-principal-color-neutral">
-      <template #title>
-        <div class="textLg flex gap-2">
-          <p>Agendar cita</p>
-        </div>
-      </template>
+    <template #title>
+      <div class="textLg flex gap-2">
+        <p>Agendar cita</p>
+      </div>
+    </template>
     <template #content>
       <Stepper value="1">
         <!-- options -->
@@ -27,9 +51,22 @@ import Button from 'primevue/button'
         <!-- panels -->
         <StepPanels>
           <!-- for pet -->
-          <StepPanel v-slot="{ activateCallback }" value="1">
-            <div class="h-48">
-              <p>Mascota</p>
+          <StepPanel class="dark:bg-surface-800" v-slot="{ activateCallback }" value="1">
+            <div class="min-h-48">
+              <p>Escoga la mascota</p>
+              <div class="w-full grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 my-4">
+                <CardPetPrimary
+                  v-for="pet in pets"
+                  :key="pet.petId"
+                  :petId="pet.petId"
+                  :petImageUrl="pet.petImageUrl"
+                  :petName="pet.petName"
+                  :petSpecie="pet.petSpecie"
+                  :petBreed="pet.petBreed"
+                  :petGender="pet.petGender"
+                  :birthdate="pet.birthdate"
+                ></CardPetPrimary>
+              </div>
             </div>
             <div class="flex justify-end">
               <Button
@@ -41,9 +78,22 @@ import Button from 'primevue/button'
             </div>
           </StepPanel>
           <!-- for service -->
-          <StepPanel v-slot="{ activateCallback }" value="2">
-            <div class="h-48">
+          <StepPanel class="dark:bg-surface-800" v-slot="{ activateCallback }" value="2">
+            <div class="min-h-48">
               <p>Servicio</p>
+              <div class="w-full grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 my-4">
+                <CardServiceSecondary
+                  v-for="service in services"
+                  :key="service.serviceId"
+                  :serviceId="service.serviceId"
+                  :serviceName="service.serviceName"
+                  :serviceImageUrl="service.serviceImageUrl"
+                  :specieName="service.specieName"
+                  :categoryName="service.categoryName"
+                  :duration="service.duration"
+                  :price="service.price"
+                ></CardServiceSecondary>
+              </div>
             </div>
             <div class="flex justify-between">
               <Button
@@ -63,9 +113,51 @@ import Button from 'primevue/button'
             </div>
           </StepPanel>
           <!-- for schedule -->
-          <StepPanel v-slot="{ activateCallback }" value="3">
-            <div class="h-48">
-              <p>Horario</p>
+          <StepPanel class="dark:bg-surface-800" v-slot="{ activateCallback }" value="3">
+            <div class="min-h-48">
+              <p>Escogue horario</p>
+              <div class="w-full flex flex-col gap-2 my-4">
+                <!-- for morning -->
+                <Panel toggleable :collapsed="true">
+                  <template #header>
+                    <div class="flex gap-2 items-center">
+                      <i class="pi pi-sun text-2xl text-yellow-500"></i>
+                      <span>Mañana</span>
+                    </div>
+                  </template>
+                  <template #icons>
+                    <span>8h-13h</span>
+                  </template>
+                  <p>Horarios mañana</p>
+                </Panel>
+                <!-- for afternoon -->
+                <Panel toggleable :collapsed="true">
+                  <template #header>
+                    <div class="flex gap-2 items-center">
+                      <i class="pi pi-cloud text-2xl text-orange-400"></i>
+                      <span>Tarde</span>
+                    </div>
+                  </template>
+                  <template #icons>
+                    <span>13h-18h</span>
+                  </template>
+                  <p>Horarios tarde</p>
+                </Panel>
+
+                <!-- night -->
+                <Panel toggleable :collapsed="true">
+                  <template #header>
+                    <div class="flex gap-2 items-center">
+                      <i class="pi pi-moon text-2xl text-sky-700"></i>
+                      <span>Noche</span>
+                    </div>
+                  </template>
+                  <template #icons>
+                    <span>18h-22h</span>
+                  </template>
+                  <p>Horarios noche</p>
+                </Panel>
+              </div>
             </div>
             <div class="flex justify-between">
               <Button
@@ -86,9 +178,9 @@ import Button from 'primevue/button'
           </StepPanel>
           <!-- for resume -->
 
-          <StepPanel v-slot="{ activateCallback }" value="4">
+          <StepPanel class="dark:bg-surface-800" v-slot="{ activateCallback }" value="4">
             <div class="h-48">
-              <p>Resument</p>
+              <p>Resumen</p>
             </div>
             <div class="flex justify-between">
               <Button
