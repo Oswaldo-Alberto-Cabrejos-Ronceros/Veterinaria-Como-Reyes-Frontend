80c93a1943c16947942f3acb2456b3602f960e78
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-06-25 20:08:58 -0500

    feat:Creacion de HeadquarterVetServiceAdapter

--- src/services/Appointment/domain/models/Appointment.ts
+++ src/services/Appointment/domain/models/Appointment.ts
@@ -1,48 +1,47 @@
-import type { Animal } from "@/services/Animal/domain/models/Animal"
-import type { Employee } from "@/services/Employee/domain/models/Employee"
-import type { VeterinaryService } from "@/services/VeterinaryService/domain/models/VeterinaryService"
+import type { Animal } from '@/services/Animal/domain/models/Animal'
+import type { Employee } from '@/services/Employee/domain/models/Employee'
+import type { VeterinaryService } from '@/services/VeterinaryService/domain/models/VeterinaryService'
 
-export interface Appointment{
-  appointmentId:number,
-  scheduleDateTime:string,
-  creationDate:string,
-  cancellationNote?:string,
-  statusAppointment:string,
-  headquarterVetService:HeadquarterVetService,
-  assignedEmployee:Employee,
-  animal:Animal
+export interface Appointment {
+  appointmentId: number
+  scheduleDateTime: string
+  creationDate: string
+  cancellationNote?: string
+  statusAppointment: string
+  headquarterVetService: HeadquarterVetService
+  assignedEmployee: Employee
+  animal: Animal
 }
 
-export interface AppointmentRequest{
-  scheduleDateTime:string,
-  comment:string,
-  headquarterVetServiceId:number,
-  assignedEmployeeId:number,
-  animalId:number,
-  paymentMethodId:number
+export interface AppointmentRequest {
+  scheduleDateTime: string
+  comment: string
+  headquarterVetServiceId: number
+  assignedEmployeeId: number
+  animalId: number
+  paymentMethodId: number
 }
 
-export interface HeadquarterVetService{
-      id:number,
-    headquarterId:number,
-    service:VeterinaryService
+export interface HeadquarterVetService {
+  id: number
+  headquarterId: number
+  service: VeterinaryService
 }
 
-export interface TimesForTurn{
-  turn:string,
-  times:FormatTime[]
+export interface TimesForTurn {
+  turn: string
+  times: FormatTime[]
 }
 
-export interface FormatTime{
-  getTime:string,
-  getTimeRange:string
+export interface FormatTime {
+  getTime: string
+  getTimeRange: string
 }
 
-
-export interface BasicServiceForAppointment{
-  headquarterServiceId:number,
-  name:string,
-  description:string,
-  price:number,
-  duration:number
+export interface BasicServiceForAppointment {
+  headquarterServiceId: number
+  name: string
+  description: string
+  price: number
+  duration: number
 }
