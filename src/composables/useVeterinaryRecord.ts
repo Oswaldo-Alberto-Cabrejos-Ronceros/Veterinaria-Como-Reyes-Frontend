0bf33d51e6121bb8715efec9632ff94875d04b39
Oswaldo <U22209742@utp.edu.pe>
2025-06-27 23:16:25 -0500

    feat:Creacion de compose para veterinaryRecord

---
+++ src/composables/useVeterinaryRecord.ts
@@ -0,0 +1,84 @@
+import type { VeterinaryRecordRequest } from '@/services/VeterinaryRecord/domain/models/VeterinaryRecord'
+import { useAsyncHandler } from './useAsyncHandler'
+import { veterinaryRecordUsesCases } from '@/dependency-injection/veterinary-record.container'
+import { VeterinaryRecordAdapter } from '@/adapters/VeterinaryRecordAdapter'
+
+export function useVeterinaryRecord() {
+  const { loading, error, runUseCase } = useAsyncHandler()
+
+  const createVeterinaryRecord = async (veterinaryRecordRequest: VeterinaryRecordRequest) => {
+    const veterinaryRecord = await runUseCase('createVeterinaryRecord', () =>
+      veterinaryRecordUsesCases.createVeterinaryRecord.execute(veterinaryRecordRequest),
+    )
+    return VeterinaryRecordAdapter.toVeterinaryRecordView(veterinaryRecord)
+  }
+
+  const findAllVeterinaryRecord = async () => {
+    const veterinaryRecords = await runUseCase('findAllVeterinaryRecord', () =>
+      veterinaryRecordUsesCases.findAllVeterinaryRecord.execute(),
+    )
+    return veterinaryRecords.map((veterinaryRecord) =>
+      VeterinaryRecordAdapter.toVeterinaryRecordView(veterinaryRecord),
+    )
+  }
+
+  const findVeterinaryRecordById = async (id: number) => {
+    const veterinaryRecord = await runUseCase('findVeterinaryRecordById', () =>
+      veterinaryRecordUsesCases.findVeterinaryRecordById.execute(id),
+    )
+    return VeterinaryRecordAdapter.toVeterinaryRecordView(veterinaryRecord)
+  }
+
+  const getAllInfoVeterinaryRecordsByPet = async (petId: number) => {
+    const veterinaryRecords = await runUseCase('getAllInfoVeterinaryRecordsByPet', () =>
+      veterinaryRecordUsesCases.getAllInfoVeterinaryRecordsByAnimal.execute(petId),
+    )
+    return veterinaryRecords.map((veterinaryRecord) =>
+      VeterinaryRecordAdapter.toVeterinaryRecordView(veterinaryRecord),
+    )
+  }
+
+  const updateVeterinaryRecord = async (
+    id: number,
+    veterinaryRecordRequest: VeterinaryRecordRequest,
+  ) => {
+    const veterinaryRecord = await runUseCase('updateVeterinaryRecord', () =>
+      veterinaryRecordUsesCases.updateVeterinaryRecord.execute(id, veterinaryRecordRequest),
+    )
+    return VeterinaryRecordAdapter.toVeterinaryRecordView(veterinaryRecord)
+  }
+
+  const setVeterinaryRecordCompletado = async (id: number) => {
+    const veterinaryRecord = await runUseCase('setVeterinaryRecordCompletado', () =>
+      veterinaryRecordUsesCases.setVeterinaryRecordCompletado.execute(id),
+    )
+    return VeterinaryRecordAdapter.toVeterinaryRecordView(veterinaryRecord)
+  }
+
+  const setVeterinaryRecordEnCurso = async (id: number) => {
+    const veterinaryRecord = await runUseCase('setVeterinaryRecordEnCurso', () =>
+      veterinaryRecordUsesCases.setVeterinaryRecordEnCurso.execute(id),
+    )
+    return VeterinaryRecordAdapter.toVeterinaryRecordView(veterinaryRecord)
+  }
+
+  const setVeterinaryRecordObservacion = async (id: number) => {
+    const veterinaryRecord = await runUseCase('setVeterinaryRecordObservacion', () =>
+      veterinaryRecordUsesCases.setVeterinaryRecordObservacion.execute(id),
+    )
+    return VeterinaryRecordAdapter.toVeterinaryRecordView(veterinaryRecord)
+  }
+
+  return {
+    loading,
+    error,
+    createVeterinaryRecord,
+    findAllVeterinaryRecord,
+    findVeterinaryRecordById,
+    getAllInfoVeterinaryRecordsByPet,
+    updateVeterinaryRecord,
+    setVeterinaryRecordCompletado,
+    setVeterinaryRecordEnCurso,
+    setVeterinaryRecordObservacion,
+  }
+}
