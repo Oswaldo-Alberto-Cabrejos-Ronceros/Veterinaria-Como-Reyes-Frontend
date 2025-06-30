239cbb0ab8bc1093db22bc3b8e996da83d985ccc
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-06-30 13:18:34 -0500

    feat:Desarrollo de casos de uso GetAllPaymentsForTable y SearchPayments en modulo pago y adicion al container

--- src/dependency-injection/payment.container.ts
+++ src/dependency-injection/payment.container.ts
@@ -3,6 +3,8 @@ import { GetPaymentById } from '@/services/Payment/aplication/GetPaymentById'
 import { CreatePayment } from '@/services/Payment/aplication/CreatePayment'
 import { UpdatePayment } from '@/services/Payment/aplication/UpdatePayment'
 import { DeletePayment } from '@/services/Payment/aplication/DeletePayment'
+import { GetAllPaymentsForTable } from '@/services/Payment/aplication/GetAllPaymentsForTable'
+import { SearchPayments } from '@/services/Payment/aplication/SearchPayments'
 
 import { AxiosHttpClient } from '@/services/Http/infrastructure/AxiosHttpClient'
 import { PaymentServiceImpl } from '@/services/Payment/infrastructure/PaymentServiceImpl'
@@ -19,5 +21,7 @@ export const paymentUsesCases = {
   getPaymentById: new GetPaymentById(paymentService),
   createPayment: new CreatePayment(paymentService),
   updatePayment: new UpdatePayment(paymentService),
-  deletePayment: new DeletePayment(paymentService)
+  deletePayment: new DeletePayment(paymentService),
+  getAllPaymentsForTable: new GetAllPaymentsForTable(paymentService),
+  searchPayments: new SearchPayments(paymentService)
 }
