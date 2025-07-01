dc119fa3180a9f50cec4a14ffb202c6441d6d69a
Oswaldo <U22209742@utp.edu.pe>
2025-06-30 21:01:35 -0500

    feat:Desarrollo de la pagina 404

---
+++ src/views/Common/PageNotFound/PageNotFound.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="fixed w-full h-screen flex items-center justify-center bg-primary-300 dark:bg-slate-800">
+    <p class=" text-6xl rotate-6 text-white">404</p>
+  </div>
+</template>
