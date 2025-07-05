e89cd8a99cd58384530caedefb52f15cb4a34d60
Oswaldo <U22209742@utp.edu.pe>
2025-07-05 13:59:42 -0500

    feat:Desarrollo de TabNav

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -14,7 +14,8 @@ import { useConfirm } from 'primevue'
 
 defineProps<{
   role: string
-  showMenu: boolean
+  showMenu?: boolean
+  isClient?:boolean
 }>()
 
 const routeMap: Record<string, string> = {
@@ -76,7 +77,7 @@ const confirmLogout = async () => {
 
 <template>
   <header class="sticky top-0 left-0 z-50">
-    <Menubar class="h-16 py-4 bg-surface-0 dark:bg-neutral-900 border-b-1 border-neutral-200 dark:border-neutral-700 rounded-none border-0">
+    <Menubar :class="['h-16 py-4 bg-surface-0 dark:bg-neutral-900 border-b-1 border-neutral-200 dark:border-neutral-700 rounded-none border-0',isClient?'px-12':'']">
       <template #start>
         <div class="flex items-center gap-0.5">
                     <Button
--- src/layouts/ClientLayout.vue
+++ src/layouts/ClientLayout.vue
@@ -2,7 +2,7 @@
 import TheHeader from '@/components/TheHeader.vue'
 import type { MenuItem } from 'primevue/menuitem'
 import { ref } from 'vue'
-import MenuNav from '@/components/MenuNav.vue'
+import TabNav from '@/components/TabNav.vue'
 
 //for menuItems
 const items = ref<MenuItem[]>([
@@ -31,11 +31,15 @@ const toggleMenu = () => {
 </script>
 <template>
   <div class="w-full min-h-screen flex flex-col">
-    <TheHeader @toggle-menu="toggleMenu" role="CLIENTE" />
+    <TheHeader is-client @toggle-menu="toggleMenu" role="CLIENTE" />
 
-    <main class="w-full flex-1 flex p-2 overflow-hidden">
-      <MenuNav :items="items" :show-menu="showMenu" @update:show-menu="showMenu = $event" />
-      <section class="flex-1 h-auto px-0.5 xs:px-4 transition-all duration-200 ease-out">
+    <main class="w-full flex-1 flex-col flex  overflow-hidden">
+      <div class="w-full px-12 p-tablist-tab-list">
+<TabNav :items="items"/>
+      </div>
+
+      <section class="flex-1 mt-4 h-auto px-0.5 xs:px-12 transition-all duration-200 ease-out">
+
         <router-view />
       </section>
     </main>
