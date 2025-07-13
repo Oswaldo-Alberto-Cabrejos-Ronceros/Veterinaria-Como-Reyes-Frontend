3d29dbf1406e4734e5cf470df5ab1c27c1f65378
Oswaldo <U22209742@utp.edu.pe>
2025-07-12 23:29:15 -0500

    feat:Logica de consumo de financialReport

---
+++ src/dependency-injection/financial-report.container.ts
@@ -0,0 +1,24 @@
+import { AxiosHttpClient } from '@/services/Http/infrastructure/AxiosHttpClient'
+import { FinancialReportServiceImpl } from '@/services/FinancialReport/infrastructure/FinancialReportServiceImpl'
+import { GetIncomeByPeriod } from '@/services/FinancialReport/aplication/GetIncomeByPeriod'
+import { GetIncomeByServicePdf } from '@/services/FinancialReport/aplication/GetIncomeByServicePdf'
+import { GetIncomeBySpeciePdf } from '@/services/FinancialReport/aplication/GetIncomeBySpeciePdf'
+import { GetIncomeByPaymentMethodExcel } from '@/services/FinancialReport/aplication/GetIncomeByPaymentMethodExcel'
+import { GetIncomeByPeriodAndServicePdf } from '@/services/FinancialReport/aplication/GetIncomeByPeriodAndServicePdf'
+import { GetIncomeByHeadquarterPdf } from '@/services/FinancialReport/aplication/GetIncomeByHeadquarterPdf'
+
+
+const httpClient = new AxiosHttpClient()
+
+const financialReportService = new FinancialReportServiceImpl(httpClient)
+
+// export uses cases
+export const financialReportUseCases = {
+  getIncomeByPeriod: new GetIncomeByPeriod(financialReportService),
+  generateIncomeByPeriodPdf: new GetIncomeByPeriod(financialReportService),
+  getIncomeByServicePdf: new GetIncomeByServicePdf(financialReportService),
+  getIncomeBySpeciePdf: new GetIncomeBySpeciePdf(financialReportService),
+  getIncomeByPaymentMethodExcel: new GetIncomeByPaymentMethodExcel(financialReportService),
+  getIncomeByPeriodAndServicePdf: new GetIncomeByPeriodAndServicePdf(financialReportService),
+  getIncomeByHeadquarterPdf: new GetIncomeByHeadquarterPdf(financialReportService)
+}
---
+++ src/services/FinancialReport/domain/services/FinantialReportService.ts
@@ -0,0 +1,10 @@
+import type { ReportPeriod } from '@/services/enums/ReportPeriod'
+
+export interface FinancialReportService {
+  getIncomeByPeriod(period: ReportPeriod): Promise<Blob>
+  getIncomeByServicePdf(): Promise<Blob>
+  getIncomeBySpeciePdf(): Promise<Blob>
+  getIncomeByPaymentMethodExcel(): Promise<Blob>
+  getIncomeByPeriodAndServicePdf(period: ReportPeriod): Promise<Blob>
+  getIncomeByHeadquarterPdf(): Promise<Blob>
+}
