bec896693928d717a45ab3f6878b1b49db861aa5
Oswaldo <U22209742@utp.edu.pe>
2025-07-12 19:11:10 -0500

    feat:Desarrollo de logica de consumo de filterHeadquarterVetServices

---
+++ src/models/HeadquarterServiceInfoPanel.ts
@@ -0,0 +1,11 @@
+export interface HeadquarterServiceInfoPanel {
+  headquarterId: number
+  headquarterName: string
+  serviceId: number
+  serviceName: string
+  serviceDescription: string
+  servicePrice: string
+  serviceDuration: string
+  categoryName: string
+  speciesName: string
+}
--- src/services/HeadquarterVetService/domain/services/HeadquarterVetServiceService.ts
+++ src/services/HeadquarterVetService/domain/services/HeadquarterVetServiceService.ts
@@ -1,4 +1,5 @@
-import type { HeadquarterVetService } from '../models/HeadquarterVetService'
+import type { PageResponse } from '@/services/models/PageResponse'
+import type { HeadquarterServiceInfoPanel, HeadquarterVetService } from '../models/HeadquarterVetService'
 import type { HeadquarterVetServiceRequest } from '../models/HeadquarterVetService'
 import type { EmployeeBasicInfo } from '@/services/Employee/domain/models/Employee'
 
@@ -6,11 +7,21 @@ export interface HeadquarterVetServiceService {
   getAll(): Promise<HeadquarterVetService[]>
   getById(id: number): Promise<HeadquarterVetService>
   getByHeadquarter(headquarterId: number): Promise<HeadquarterVetService[]>
-    getAllHeadquarterVetServiceByHeadquarter(headquarterId: number): Promise<HeadquarterVetService[]>
+  getAllHeadquarterVetServiceByHeadquarter(headquarterId: number): Promise<HeadquarterVetService[]>
   create(request: HeadquarterVetServiceRequest): Promise<HeadquarterVetService>
   update(id: number, request: HeadquarterVetServiceRequest): Promise<HeadquarterVetService>
   delete(id: number): Promise<void>
   listVeterinariansByHeadVetService(headquarterVetServiceId: number): Promise<EmployeeBasicInfo[]>
   enableHeadquarterVetService(id: number): Promise<void>
   updateSimultaneousCapacity(id: number, capacity: number): Promise<void>
+  filterHeadquarterServices(
+    page: number,
+    size: number,
+    filters: {
+      serviceName?: string
+      categoryId?: number
+      speciesId?: number
+      headquarterId?: number
+    },
+  ): Promise<PageResponse<HeadquarterServiceInfoPanel>>
 }
